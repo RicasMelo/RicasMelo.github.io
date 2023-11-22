f4985387384134c7fdc51e18730f5d695adf8422
RicasMelo <74099294+RicasMelo@users.noreply.github.com>
2023-11-22 22:32:15 +0000

    public again

--- app.js
+++ app.js
@@ -4,16 +4,16 @@ const path = require('path');
 const app = express();
 
 // Serve static files from the public directory
-app.use(express.static('docs'));
+app.use(express.static('public'));
 
 // Route for '/'
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'docs', 'index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 // Route for '/teste'
 app.get('/teste', (req, res) => {
-    res.sendFile(path.join(__dirname, 'docs', 'teste.html'));
+    res.sendFile(path.join(__dirname, 'public', 'teste.html'));
 });
 
 const PORT = process.env.PORT || 3000;
